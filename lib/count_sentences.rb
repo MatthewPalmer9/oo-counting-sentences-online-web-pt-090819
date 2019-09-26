9c49043e48442ea8e6c56f94a9fab095db0a23cd
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-26 14:36:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    newArray = self.split()
+    count = 0 
+    newArray.each(){|i| 
+      count += 1 if i.include?("." || "?" ||"!")
+    }
+    return count 
   end
 end
